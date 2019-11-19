f8cbd8f6ff79765e2b23db53b40c75268c504a6d
jahir Moncada <geminis31_98@hotmail.com>
2019-11-19 04:00:08 -0500

    vendor

---
+++ vendor/cakephp-plugins.php
@@ -0,0 +1,12 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+return [
+    'plugins' => [
+        'AdminLTE' => $baseDir . '/vendor/maiconpinto/cakephp-adminlte-theme/',
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
+        'Proffer' => $baseDir . '/vendor/davidyell/proffer/',
+        'WyriHaximus/TwigView' => $baseDir . '/vendor/wyrihaximus/twig-view/'
+    ]
+];
---
+++ vendor/maiconpinto/cakephp-adminlte-theme/src/Template/Element/footer.ctp
@@ -0,0 +1,12 @@
+<footer class="main-footer">
+  <?php if (isset($layout) && $layout == 'top'): ?>
+  <div class="container">
+  <?php endif; ?>
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.4.5
+    </div>
+    <strong>Copyright &copy; 2019 </strong> Dermiapp
+  <?php if (isset($layout) && $layout == 'top'): ?>
+  </div>
+  <?php endif; ?>
+</footer>
